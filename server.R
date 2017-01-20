2a18280a9455112ca7e57a498a5b6cb44a5aba34
Ian Wilson <wilson.ianj@gmail.com>
2017-01-20 11:10:55 +0000

    some minor tidying

--- server.R
+++ server.R
@@ -5,21 +5,6 @@
 # http://shiny.rstudio.com
 #
 
-
-
-### Plotfreqs should beony be called when we have
-### either a) A new set of parameters and only one set of frequencies
-### or b) We have added another set of data
-# plotfreqs <- function() {
-#   ngens <- length(freqs[[1]])
-#   plot(1:ngens, freqs[[1]], type="l", axes=FALSE)
-#   axis(1)
-#   axis(2)
-#   if (length(freqs)>1)
-#     lapply(freqs[-1], function(x) lines(1:ngens, x) )
-# }
-
-
 library(shiny)
 library(rje)    ## for colours
 
@@ -42,7 +27,6 @@ shinyServer(function(input, output) {
     layout(matrix(c(1,2), ncol=1), heights=c(3,1))
     freq <- numeric(gens)
     plot(1, type="n", ylim=c(1-0.5, n+0.5), xlim=c(0.2, gens), axes=FALSE, ylab="", xlab="")
-    #axis(1, at=1:gens - 0.5, labels=1:gens, tick=FALSE, cex=1.2)
     ybottom <- seq((1:n))-0.3
     ytop <- ybottom + 0.6
     ## initial population
@@ -58,18 +42,19 @@ shinyServer(function(input, output) {
       prev <- curr
       freq[i] <- sum(curr<=n/2)/n
     }
+    ## The second plot
     par(mar=c(2, 2, 0, 0), mgp=c(1, 0.5, 0))      ## fix margins
     colours <- rep(1, gens)
     colours[freq==0] <- 2
     colours[freq==1] <- 3
+    
     plot(NULL, xlim=c(0.2, gens), ylim=c(0,1), axes=FALSE,xlab="",ylab="")
     segments(0,0,gens,0, col="lightgrey")
     segments(0,0.5,gens,0.5, col="lightgrey")
     segments(0, 1,gens, 1, col="lightgrey")
     points((1:gens)-0.5, freq, type="b",col=colours, cex=2, pch=18)
 
-       axis(1, at=1:gens - 0.5, labels=1:gens, tick=FALSE, cex=1.2)
-       
+    axis(1, at=1:gens - 0.5, labels=1:gens, tick=FALSE, cex=1.2)
     axis(2, at=c(0,0.5,1))
   })
 })
